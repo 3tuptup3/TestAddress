417ba7b989cb4d7522951ad9f0957a129d1f2acf
water <water@taputapu>
2025-02-07 17:38:25 +0900

    test

--- src/main/java/com/example/demo/controller/CommonController.java
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CommonController {
 	
 	// 完了後のリダイレクト先
-	// リダイレクトでPOSTリクエストではなくGETリクエストとしてHTMLを返す
 	@GetMapping("/complete")
 	private String complate() {
 		return "complete";
